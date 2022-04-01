d36715355c460636e9397cbc7ed3b7c237edf9c3
bwz <549068446@qq.com>
2022-04-01 14:27:28 +0800

    pth2onnx优化

--- pth2onnx.sh
+++ pth2onnx.sh
@@ -1,7 +1,17 @@
+exp_name='voc_ocr_flip'
+out_name='yolox_flip1'
+
 python tools/export_onnx.py \
--c /home/ecnu-lzw/bwz/ocr-gy/YOLOX/YOLOX_outputs/voc_ocr_2023/best_ckpt.pth \
--f /home/ecnu-lzw/bwz/ocr-gy/YOLOX/myconfig/voc_ocr_2023.py \
---output-name=yolox_s2023_encodedout.onnx \
+-c /home/ecnu-lzw/bwz/ocr-gy/YOLOX/YOLOX_outputs/${exp_name}/best_ckpt.pth \
+-f /home/ecnu-lzw/bwz/ocr-gy/YOLOX/myconfig/${exp_name}.py \
+--output-name=${out_name}_decodedout.onnx \
+--dynamic --no-onnxsim \
+--decode_in_inference
+
+python tools/export_onnx.py \
+-c /home/ecnu-lzw/bwz/ocr-gy/YOLOX/YOLOX_outputs/${exp_name}/best_ckpt.pth \
+-f /home/ecnu-lzw/bwz/ocr-gy/YOLOX/myconfig/${exp_name}.py \
+--output-name=${out_name}_encodedout.onnx \
 --dynamic --no-onnxsim \
 # --decode_in_inference
 
--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
 python tools/train.py \
--f /home/ecnu-lzw/bwz/ocr-gy/YOLOX/myconfig/voc_ocr_2023.py \
+-f /home/ecnu-lzw/bwz/ocr-gy/YOLOX/myconfig/voc_ocr_flip.py \
 -d 2 -b 64 --fp16 -o \
 -c /home/ecnu-lzw/bwz/ocr-gy/YOLOX/YOLOX_outputs/voc_ocr_2022/best_ckpt.pth \
 --cache
 
 # /home/ecnu-lzw/bwz/ocr-gy/YOLOX/train.sh
-# tensorboard --logdir='/home/ecnu-lzw/bwz/ocr-gy/YOLOX/YOLOX_outputs/voc_ocr_2023'
+# tensorboard --logdir='/home/ecnu-lzw/bwz/ocr-gy/YOLOX/YOLOX_outputs/voc_ocr_flip'
 
 # tmux new -s bwz
 # tmux detach  //离开终端后台运行
